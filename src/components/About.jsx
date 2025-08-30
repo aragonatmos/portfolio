b9903578ee5b4c354d53ff2a2cf431ee3899cc72
Jared Aragon <aragonatmos@gmail.com>
2025-08-29 21:22:07 -0400

    add classname to div in About component, add style to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,15 +4,15 @@ import Picture from './images/profile-img.jpg';
 
 function About() {
     return (
-        <div>
-            <img class="profile-pic" src={Picture} alt="man in front of a pine tree" />
+        <div className ='business-card'>
+            <img class="profile-pic" src={Picture} alt="picture of a full-stack web developer" />
             <ul>
                 <h2 className="bio-title">Jared Aragon</h2>
-                <li className="bio-text">Email: jaredaragon7@gmail.com</li>
                 <li href="https://github.com/aragonatmos" className="bio-text">Grade my code please! - Github Account </li>
                 <li className="bio-text" >Sparta, NJ</li>
             </ul>
             <p className="bio-text">Full-Stack Web Developer</p>
+            <p className="bio-text">Email: jaredaragon7@gmail.com</p>
         </div>
     );
 }
